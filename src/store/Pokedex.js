1c7d94f85d53b43060b7ce02426adb34491e4dd8
Jet Lawrence P. Doctor <jetlawrence@users.noreply.github.com>
2018-05-17 13:20:39 +0800

    Update Pokedex to fetch and set pokemon data

--- src/store/Pokedex.js
+++ src/store/Pokedex.js
@@ -2,14 +2,29 @@
 
 import { action, observable, IObservableArray } from 'mobx';
 import Pokemon from '../entities/Pokemon';
-import type PokeAPIClient from '../PokeAPIClient';
+import PokeAPIClient from '../PokeAPIClient';
+
+type PokemonInitialRawData = {
+  url: string,
+  name: string,
+};
+
+type PokemonListRawData = {
+  count: number,
+  previous: string | null,
+  results: Array<PokemonInitialRawData>,
+  next: string | null,
+};
 
 export default class Pokedex {
-  @observable currentPokemons: IObservableArray<Pokemon> = observable([]);
+  @observable pokemonsCache: IObservableArray<Pokemon> = observable([]);
+  @observable currentDisplayedPokemons: IObservableArray<Pokemon> = observable([]);
   @observable isLoading: boolean = false;
   @observable nextPageURL: string;
   @observable previousPageURL: string;
   @observable isErrorLoading: boolean = false;
+
+  static NUM_OF_PKMNS_PER_PAGE = 20;
 
   pokeAPIClient: PokeAPIClient;
 
@@ -18,8 +33,13 @@ export default class Pokedex {
   }
 
   @action
-  setCurrentPokemons(currentPokemons: Array<Pokemon>) {
-    this.currentPokemons.replace(currentPokemons);
+  setCurrentDisplayedPokemons(currentDisplayedPokemons: Array<Pokemon>) {
+    this.currentDisplayedPokemons.replace(currentDisplayedPokemons);
+  }
+
+  @action
+  cachePokemon(pokemon: Pokemon) {
+    this.pokemonsCache.push(pokemon);
   }
 
   @action
@@ -54,27 +74,31 @@ export default class Pokedex {
 
   async fetchPokemons({
     shouldFetchNextPage,
-    shouldPreviousPage,
+    shouldFetchPreviousPage,
   }: {
       shouldFetchNextPage?: boolean,
-      shouldPreviousPage?: boolean,
-    } = { shouldFetchNextPage: false, shouldPreviousPage: false }) {
+      shouldFetchPreviousPage?: boolean,
+    } = { shouldFetchNextPage: false, shouldFetchPreviousPage: false }) {
     try {
       this.startLoading();
 
       const { getPokemonDataByURL, getPokemonList } = this.pokeAPIClient;
-
       let pokemonListData = null;
+      const nextPage = shouldFetchNextPage && this.nextPageURL;
+      const prevPage = shouldFetchPreviousPage && this.previousPageURL;
 
-      if (shouldFetchNextPage) {
+      if (nextPage) {
         pokemonListData = await getPokemonDataByURL(this.nextPageURL);
-      } else if (shouldPreviousPage) {
+      } else if (prevPage) {
         pokemonListData = await getPokemonDataByURL(this.previousPageURL);
       } else {
-        pokemonListData = await getPokemonList({ limit: 20 });
+        pokemonListData = await getPokemonList({
+          limit: Pokedex.NUM_OF_PKMNS_PER_PAGE,
+        });
       }
 
       if (pokemonListData) {
+        this.processPokemonListRawData(pokemonListData);
         this.successLoading();
       } else {
         this.errorLoading();
@@ -84,5 +108,47 @@ export default class Pokedex {
     }
 
     this.finishLoading();
+  }
+
+  async loadFullPokemonData(pokemon: Pokemon, url: string) {
+    try {
+      const fullPokemonData = await this.pokeAPIClient.getPokemonDataByURL(url);
+      const spriteData = await this.pokeAPIClient.getPokemonDataByURL(fullPokemonData.forms[0]);
+      const sprite = spriteData.sprites.front_default;
+
+      pokemon.setPokemonData({ sprite });
+    } catch (error) {
+      console.log(`Error loading data for ${pokemon.name}`);
+    }
+  }
+
+  processPokemonListRawData(pokemonListData: PokemonListRawData) {
+    const pokemons = this.formatPokemonListRawData(pokemonListData);
+    this.setCurrentDisplayedPokemons(pokemons);
+  }
+
+  formatPokemonListRawData(pokemonListData: PokemonListRawData): Array<Pokemon> {
+    return pokemonListData.results.map(this.getPokemonFromRawData);
+  }
+
+  getPokemonFromRawData = (pokemonInitialRawData: PokemonInitialRawData): Pokemon => {
+    const { name, url } = pokemonInitialRawData;
+    const cachedPokemon = this.getCachedPokemon(name);
+    const pokemon = cachedPokemon || new Pokemon(name);
+
+    this.loadFullPokemonData(pokemon, url);
+
+    if (!cachedPokemon) {
+      this.cachePokemon(pokemon);
+    }
+
+    return pokemon;
+  };
+
+  getCachedPokemon(name: string): ?Pokemon {
+    const isCached = (cachedPokemon: Pokemon) =>
+      cachedPokemon.name.toLowerCase() === name.toLowerCase();
+
+    return this.pokemonsCache.find(isCached);
   }
 }
