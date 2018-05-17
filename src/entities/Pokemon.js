ed0ad9bcb21a6f0dae589f175afef65b6a1af848
Jet Lawrence P. Doctor <jetlawrence@users.noreply.github.com>
2018-05-17 13:18:39 +0800

    Update entity

--- src/entities/Pokemon.js
+++ src/entities/Pokemon.js
@@ -2,22 +2,25 @@
 
 import { action, observable } from 'mobx';
 
-type PokemonParams = {
-  name: string,
-  apiURL: string,
+type PokemonData = {
+  sprite?: any,
 };
 
 export default class Pokemon {
   @observable name: string = '';
-  @observable apiURL: string = '';
+  @observable sprite: string = '';
 
-  constructor(pokemonParams: PokemonParams) {
-    this.setParams(pokemonParams);
+  constructor(name: string) {
+    this.setName(name);
   }
 
   @action
-  setParams(pokemonProperties: PokemonParams) {
-    this.name = pokemonProperties.name;
-    this.apiURL = pokemonProperties.apiURL;
+  setName(name: string) {
+    this.name = name;
+  }
+
+  @action
+  setPokemonData(pokemonData: PokemonData) {
+    this.sprite = pokemonData.sprite || this.sprite;
   }
 }
